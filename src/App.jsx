1f033a84d57872bda33e133f44fe306974b0c412
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-10-22 11:53:14 -0300

    :construction: feat: adicionando o background e normalize css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
+import styled from "styled-components";
+import EstilosGlobais from "./components/EstilosGlobais/EstilosGlobais";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  height: 100%;
+  width: 100%;
+  min-height: 100vh;
+  min-width: 100vh;
+`;
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <FundoGradiente>
+      <EstilosGlobais />
+    </FundoGradiente>
+  );
 }
 
-export default App
+export default App;
