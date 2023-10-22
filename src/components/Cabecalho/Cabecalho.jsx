b5d782a20992409c8aee962a15f96ef275a058c9
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-10-22 15:07:04 -0300

    :lipstick: Adicionando imagens da aplicação

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderStyle = styled.header`
+  padding: 60px, 0;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Cabecalho = () => {
+  return (
+    <HeaderStyle>
+      <img src="" alt="" />
+    </HeaderStyle>
+  )
+}
+
+export default Cabecalho
