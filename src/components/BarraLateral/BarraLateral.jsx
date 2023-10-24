c0fb00a064c6fcac050d0806e375c682ac405c73
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-10-23 22:29:53 -0300

    :sparkles: feat: galeria adicionada

--- src/components/BarraLateral/BarraLateral.jsx
+++ src/components/BarraLateral/BarraLateral.jsx
@@ -14,19 +14,19 @@ const BarraLateral = () => {
     <aside>
       <nav>
         <ListaEstilizada>
-          <ItemNavagacao iconeAtivo="./src/assets/icones/home-ativo.png" iconeInativo="./src/assets/icones/home-inativo.png" ativo= { true}>
+          <ItemNavagacao iconeAtivo="./public/icones/home-ativo.png" iconeInativo="./public/icones/home-inativo.png" ativo= { true}>
             Inicio
           </ItemNavagacao>
-          <ItemNavagacao iconeAtivo="./src/assets/icones/mais-vistas-ativo.png" iconeInativo="./src/assets/icones/mais-vistas-inativo.png">
+          <ItemNavagacao iconeAtivo="./public/icones/mais-vistas-ativo.png" iconeInativo="./public/icones/mais-vistas-inativo.png">
             Mais vistas
           </ItemNavagacao>
-          <ItemNavagacao iconeAtivo="./src/assets/icones/mais-curtidas-ativo.png" iconeInativo="./src/assets/icones/mais-curtidas-inativo.png">
+          <ItemNavagacao iconeAtivo="./public/icones/mais-curtidas-ativo.png" iconeInativo="./public/icones/mais-curtidas-inativo.png">
             Mais curtidas
           </ItemNavagacao>
-          <ItemNavagacao iconeAtivo="./src/assets/icones/novas-ativo.png" iconeInativo="./src/assets/icones/novas-inativo.png">
+          <ItemNavagacao iconeAtivo="./public/icones/novas-ativo.png" iconeInativo="./public/icones/novas-inativo.png">
             Novas
           </ItemNavagacao>
-          <ItemNavagacao iconeAtivo="./src/assets/icones/surpreenda-me-ativo.png" iconeInativo="./src/assets/icones/surpreenda-me-inativo.png">
+          <ItemNavagacao iconeAtivo="./public/icones/surpreenda-me-ativo.png" iconeInativo="./public/icones/surpreenda-me-inativo.png">
             Supreenda-me
           </ItemNavagacao>
         </ListaEstilizada>
--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -3,26 +3,36 @@ import Titulo from "../Titulo/Titulo";
 import Tags from "./Tags/Tags";
 import Populares from "./Populares/Populares";
 import styled from "styled-components";
+import Imagem from "./Imagem/Imagem";
 
 const GaleriaContainer = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 const SecaoFluida = styled.section`
-    flex-grow: 1;
+  flex-grow: 1;
+`;
+
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
 `
-const Galeria = ({fotos = []}) => {
+const Galeria = ({ fotos = [] }) => {
   return (
     <>
       <Tags />
       <GaleriaContainer>
         <SecaoFluida>
           <Titulo>Navegue pela galeria</Titulo>
-          <ul>
-            {fotos.map((foto) => foto.titulo)}
-          </ul>
+          <ImagensContainer>
+            {fotos.map((foto) => (
+              <Imagem key={foto.id} foto={foto} />
+            ))}
+          </ImagensContainer>
         </SecaoFluida>
-        <Populares/>
+        <Populares />
       </GaleriaContainer>
     </>
   );
